d91633492f7cc29d7adf797f473d24121d23aa28
blorne <bjorn.j.hansson@gmail.com>
2014-10-01 18:06:34 +0200

    Update
    Added controllerclasses and implemented mouslistener

--- src/gui/SlotLabel.java
+++ src/gui/SlotLabel.java
@@ -1,9 +1,55 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class SlotLabel extends ColoredLabel {
-    public SlotLabel() {
+import javax.swing.JLabel;
+
+public class SlotLabel extends ColoredLabel implements MouseListener {
+    private String position;
+	
+	public SlotLabel(String position) {
         super("                    ", Color.WHITE, RIGHT);
+        addMouseListener(this);
+        this.position = position;
     }
-}
+
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		System.out.println("Hej");
+		super.setText(position);
+		
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}	
