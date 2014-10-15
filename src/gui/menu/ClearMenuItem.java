78da87d2d92f8efa91a9eb238767d8567bf1cdaf
m-tovar <marceldavidtovar@hotmail.com>
2014-10-15 16:34:24 +0200

    clear menu item

--- src/gui/menu/ClearMenuItem.java
+++ src/gui/menu/ClearMenuItem.java
@@ -1,16 +1,26 @@
 package gui.menu;
 
+import gui.CurrentSlotIndicator;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JMenuItem;
 
+import controller.Model;
+
 class ClearMenuItem extends JMenuItem implements ActionListener {
-    public ClearMenuItem() {
+	Model m;
+	CurrentSlotIndicator currentSlot;
+	
+    public ClearMenuItem(Model m, CurrentSlotIndicator currentSlot) {
         super("Clear");
         addActionListener(this);
+        this.m = m;
+        this.currentSlot = currentSlot;
     }
 
     public void actionPerformed(ActionEvent e) {
-        // TODO
+        m.remove(currentSlot.getName());
     }
 }
